2793fecb78ec39dcddbbb26b057732129977b89f
Mark Florian <markrian@gmail.com>
2018-08-16 10:59:13 +0100

    random progress

--- index.js
+++ index.js
@@ -1,24 +1,93 @@
-var map = L.map('fractal', {
+function Deferred() {
+    const deferred = { promise: null, resolve: noop, reject: noop };
+    deferred.promise = new Promise((resolve, reject) => {
+        deferred.resolve = resolve;
+        deferred.reject = reject;
+    });
+
+    return deferred;
+}
+
+class MandelbrotRenderer {
+    constructor(numWorkers) {
+        this._deferreds = new Map();
+        this._workers = [];
+        const boundOnMessage = this.onMessage.bind(this);
+        while (numWorkers--) {
+            const worker = new Worker('worker.js');
+            worker.onmessage = boundOnMessage;
+            worker.idle = true;
+            this._workers.push(worker);
+        }
+    }
+
+    getImageData(coords, tileSize, iterations) {
+        const deferred = Deferred();
+        this._deferreds.push(deferred);
+
+        // try to send message to idle worker, if any
+        for (const worker of this._workers) {
+            if (worker.idle) {
+                worker.idle = false;
+                worker.postMessage({
+                    id: ++this._id,
+                    coords,
+                    tileSize,
+                    iterations,
+                });
+                break;
+            }
+        }
+
+        return deferred.promise;
+    }
+
+    onMessage(event) {
+        const worker = event.target;
+        if (worker.idle === false) {
+            console.warn('just received message from non-idle worker - should never happen?');
+        }
+
+        // resolve deferred
+        const deferred = this._deferreds.get(event.data.id);
+        if (deferred !== undefined) {
+            deferred.resolve(event.data.imageData);
+            this._deferreds.delete(event.data.id);
+        }
+
+        // post new message if any deferreds still present
+        const nextJob = this.getNextJob();
+        if (nextJob !== undefined && worker.idle) {
+            worker.idle = false;
+            worker.postMessage(nextJob.message);
+        }
+    }
+
+    getNextJob() {
+        for (const job of this._jobs.values()) {
+            if (!job.posted) {
+                return job;
+            }
+        }
+    }
+}
+
+const map = L.map('fractal', {
     center: [0, 0],
-    zoom: 0
+    zoom: 0,
 });
 
-L.GridLayer.DebugCoords = L.GridLayer.extend({
-    createTile: function (coords, done) {
-        var tile = document.createElement('div');
-        tile.innerHTML = [coords.x, coords.y, coords.z].join(', ');
-        tile.style.outline = '1px solid red';
+L.GridLayer.MandelbrotLayer = L.GridLayer.extend({
+    createTile(coords, done) {
+        const tileSize = this.getTileSize();
+        const tile = document.createElement('canvas');
+        const ctx = tile.getContext('2d');
 
-        setTimeout(function () {
-            done(null, tile);	// Syntax is 'done(error, tile)'
-        }, 500 + Math.random() * 1500);
+        mandelbrotRenderer.getImageData(coords, tileSize, iterations)
+            .then(imageData => ctx.putImageData(imageData, 0, 0));
 
         return tile;
     }
 });
 
-L.gridLayer.debugCoords = function (opts) {
-    return new L.GridLayer.DebugCoords(opts);
-};
-
-map.addLayer(L.gridLayer.debugCoords());
+map.addLayer(new L.GridLayer.MandelbrotLayer());
